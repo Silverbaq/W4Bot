a169a777ba6ff3562012f3c39aaf9a6ecfff562d
Steffen Mogensen <feber.sm@gmail.com>
2018-08-11 23:20:22 +0200

    added base64 encode/decode

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
+import utils
 
 bot = commands.Bot(command_prefix='!')
 
@@ -15,6 +16,11 @@ async def on_ready():
     print('Ready when you are')
     print('I am running on ' + bot.user.name)
     print('With the ID: ' + bot.user.id)
+
+
+@bot.event
+async def on_member_join(user: discord.Member):
+    await bot.say("Hi {0}! Great to see you!".format(user.name))
 
 
 @bot.command(pass_context=True)
@@ -29,6 +35,19 @@ async def info(ctx, user: discord.Member):
     await bot.say("The users status is: {0}".format(user.status))
     await bot.say("The users highest role is: {0}".format(user.top_role))
     await bot.say("The user joined at: {0}".format(user.joined_at))
+
+
+######## Encoding / Decoding ########
+@bot.command(pass_context=True)
+async def b64encode(ctx, *text):
+    res = utils.encode_base64(' '.join(text))
+    await bot.say(res)
+
+
+@bot.command(pass_context=True)
+async def b64decode(ctx, *text):
+    res = utils.decode_base64(' '.join(text))
+    await bot.say(res)
 
 
 bot.run(config('TOKEN'))
---
+++ utils.py
@@ -0,0 +1,7 @@
+import base64
+
+def encode_base64(data):
+    return base64.b64encode(data.encode('utf-8')).decode('utf-8')
+
+def decode_base64(data):
+    return base64.b64decode(data).decode('utf-8')
